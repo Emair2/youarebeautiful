d29417d075aa22ae92e4b15f009ce0f4eadeeaf0
Emair2 <yzhu28@saic.edu>
2022-04-13 12:20:12 -0500

    Update sketch.js

--- YRB-2/sketch.js
+++ YRB-2/sketch.js
@@ -7,7 +7,8 @@
 // // let detector;
 let model;
 let faces;
-
+let capture;
+let previousPixels;
 const w = 640;
 const h = 480;
 
@@ -25,16 +26,24 @@ function setup() {
 
 
 function draw() {
-    background(200);
+    background(100, 255, 255);
     if (capture.loadedmetadata && model !== undefined) {
         getFaces();
     }
 
-    push();
-        translate(w, 0);
-        scale(-1, 1);
-        image(capture, 0, 0);
-    pop();
+    // let totalMotion = detectMotion();
+
+    // if(totalMotion > 30000) {
+    //   alert("movement!")
+    // }
+    // push();
+    //     translate(w, 0);
+    //     scale(-1, 1);
+    //     image(capture, 0, 0);
+    // pop();
+
+    // fill(100, 255, 255);
+    // rect(0,0,640,480);
 
     noStroke();
     if(faces !== undefined) {
@@ -162,7 +171,7 @@ function draw() {
           }
           
           pop();
-          
+
         }
     }
 }
@@ -243,4 +252,55 @@ async function getFaces() {
     } else {
         faces = predictions;
     }
+}
+
+function detectMotion() {
+  capture.loadPixels();
+    var total = 0;
+    if (capture.pixels.length > 0) { // don't forget this!
+        if (!previousPixels) {
+            previousPixels = copyImage(capture.pixels, previousPixels);
+        } else {
+            var w = capture.width,
+                h = capture.height;
+            var i = 0;
+            var pixels = capture.pixels;
+            var thresholdAmount = select('#thresholdAmount').value() * 255. / 100.;
+            thresholdAmount *= 3; // 3 for r, g, b
+            for (var y = 0; y < h; y++) {
+                for (var x = 0; x < w; x++) {
+                    // calculate the differences
+                    var rdiff = Math.abs(pixels[i + 0] - previousPixels[i + 0]);
+                    var gdiff = Math.abs(pixels[i + 1] - previousPixels[i + 1]);
+                    var bdiff = Math.abs(pixels[i + 2] - previousPixels[i + 2]);
+                    // copy the current pixels to previousPixels
+                    previousPixels[i + 0] = pixels[i + 0];
+                    previousPixels[i + 1] = pixels[i + 1];
+                    previousPixels[i + 2] = pixels[i + 2];
+                    var diffs = rdiff + gdiff + bdiff;
+                    var output = 0;
+                    if (diffs > thresholdAmount) {
+                        output = 255;
+                        total += diffs;
+                    }
+                    pixels[i++] = output;
+                    pixels[i++] = output;
+                    pixels[i++] = output;
+                    // also try this:
+                    // pixels[i++] = rdiff;
+                    // pixels[i++] = gdiff;
+                    // pixels[i++] = bdiff;
+                    i++; // skip alpha
+                }
+            }
+        }
+    }
+    // need this because sometimes the frames are repeated
+    if (total > 0) {
+        //select('#motion').elt.innerText = total;
+        capture.updatePixels();
+        //image(capture, 0, 0, 640, 480);
+    }
+
+    return total;
 }
